f0ad09723c27427a98b28f4bb974fb68eea4867f
DC23 <jugglindan@gmail.com>
2018-09-08 23:08:20 +0800

    bootstrap

--- ccc_gui/app.py
+++ ccc_gui/app.py
@@ -43,6 +43,7 @@ external_js = []
 
 external_css = [
     # dash stylesheet
+    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
     "https://codepen.io/chriddyp/pen/bWLwgP.css",
     # "https://fonts.googleapis.com/css?family=Lobster|Raleway",
     # "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
