78e47e03fd1745d39817b09710f1914a344918e0
Daniel Collins <daniel.collins@csiro.au>
2018-09-10 15:38:36 +0800

    started fixing page layout. switched to black formatter

--- ccc_gui/app.py
+++ ccc_gui/app.py
@@ -1,132 +1,117 @@
-'''Main app for ccc GUI'''
+"""Main app for ccc GUI"""
 
-# pylint: disable=C0103
+# pylama: ignore=W0611
 from exceptions import ImproperlyConfigured
 import os
-# import dash_core_components as dcc
+import dash_core_components as dcc
 import dash_html_components as html
 import plotly.plotly as py
-# import plotly.graph_objs as go
+import plotly.graph_objs as go
 from flask import Flask
 from dash import Dash
-# from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, State
 from dotenv import load_dotenv
 
-DOTENV_PATH = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(DOTENV_PATH)
-
-# Heroku-specific config
-if 'DYNO' in os.environ:
+if "DYNO" in os.environ:
+    # Heroku-specific config
     debug = False
 else:
+    # Development-mode: set debug to true and load from .env file
     debug = True
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
     load_dotenv(dotenv_path)
 
 # Sign in to plotly
 try:
-    py.sign_in(os.environ['PLOTLY_USERNAME'], os.environ['PLOTLY_API_KEY'])
+    py.sign_in(os.environ["PLOTLY_USERNAME"], os.environ["PLOTLY_API_KEY"])
 except KeyError:
-    raise ImproperlyConfigured('Plotly credentials not set in .env')
+    raise ImproperlyConfigured("Plotly credentials not set in .env")
 
 # app init
-app_name = 'Car Cost Calculator GUI'
+app_name = "Car Cost Calculator GUI"
 server = Flask(app_name)
 
 try:
-    server.secret_key = os.environ['SECRET_KEY']
+    server.secret_key = os.environ["SECRET_KEY"]
 except KeyError:
-    raise ImproperlyConfigured('SECRET KEY not set in .env:')
+    raise ImproperlyConfigured("SECRET KEY not set in .env:")
 
-app = Dash(name=app_name, server=server, csrf_protect=False)
+app = Dash(name=app_name, server=server, csrf_protect=True)
 
 external_js = []
 
 external_css = [
-    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+    "https://codepen.io/chriddyp/pen/bWLwgP.css",
 ]
 
 
 def create_header():
-    '''page header'''
-    header = html.Header(
-        html.H1(
-            children=app_name,
-            # className='well',
-            id='header',
-            style={},
-        ))
+    """page header"""
+    header = html.Header(html.H1(app_name, id="header", className="well", style={}))
     return header
 
 
 def create_content():
-    '''page content'''
+    """page content"""
     inputs = html.Div(
-        className='col-lg-5',
-        children=[
-            '''Laboris adipisicing enim do ipsum sint adipisicing irure elit labore.
-            Ea nisi sint irure ullamco non.'''
-        ])
+        [
+            """Laboris adipisicing enim do ipsum sint adipisicing irure elit labore. Ea nisi sint irure ullamco non."""
+        ],
+        className="col-lg-5",
+    )
 
     outputs = html.Div(
-        className='col-lg-7',
-        children=[
-            '''Do incididunt ipsum exercitation enim eiusmod anim. Ex qui
+        [
+            """Do incididunt ipsum exercitation enim eiusmod anim. Ex qui
             tempor excepteur in ea magna nulla Lorem nulla sint labore
             proident qui. Ex anim minim quis labore non qui Lorem. Ad duis
             anim officia est culpa excepteur proident officia excepteur
             laborum non. Tempor excepteur est ipsum do dolore nulla ut ipsum.
-            Sint elit non excepteur tempor amet. Duis sunt commodo id id.''',
-        ])
+            Sint elit non excepteur tempor amet. Duis sunt commodo id id."""
+        ],
+        className="col-lg-7",
+    )
 
-    content = html.Div(
-        id='content', className='row', children=[
-            inputs,
-            outputs,
-        ])
+    content = html.Div([inputs, outputs], id="content", className="row")
 
     return content
 
 
 def create_footer():
-    '''page footer'''
-    p0 = html.P(children=[
-        html.Span('Built with '),
-        html.A(
-            'Plotly Dash',
-            href='https://github.com/plotly/dash',
-            target='_blank'),
-    ])
-
-    footer = html.Footer(
-        children=html.Div([p0]),
-        className='footer navbar-fixed-bottom',
-        style={})
+    """page footer"""
+    footer = html.Div(
+        [
+            html.Div(
+                [
+                    html.P("Footer text - LHS", className="navbar-text pull-left"),
+                    html.P("Footer text - RHS", className="navbar-text pull-right"),
+                ],
+                className="container",
+            )
+        ],
+        className="navbar navbar-default navbar-fixed-bottom",
+    )
     return footer
 
 
 def serve_layout():
-    '''page layout function'''
-    print('serve page')
+    """page layout function"""
+    print("serve page")
     layout = html.Div(
-        children=[create_header(),
-                  create_content(),
-                  create_footer()],
-        className='container',
-        style={},
+        [create_header(), create_content(), create_footer()], className="container"
     )
     return layout
 
 
 app.layout = serve_layout
 for js in external_js:
-    app.scripts.append_script({'external_url': js})
+    app.scripts.append_script({"external_url": js})
 for css in external_css:
-    app.css.append_css({'external_url': css})
+    app.css.append_css({"external_url": css})
 
 # TODO: callbacks
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run_server(debug=debug, port=port, threaded=True)
